2f84a05b0226acb6e8c35747877816bf988e470e
Aleksey Komissarov <ad3002@gmail.com>
2024-12-01 02:05:15 +0200

    Add torch import to main.py for enhanced functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import warnings
 warnings.filterwarnings('ignore', category=UserWarning)
 warnings.filterwarnings('ignore', category=FutureWarning)
 
+import torch  # Add this import
 import argparse
 from transformers import AutoTokenizer, AutoModel, DataCollatorWithPadding, AutoConfig
 from dnaCLIP.core.base_classes import BaseDNAModel, BaseTrainer
